beef623cca274696209218e7075fd4afed225e2f
jdasol406 <jdasol406@gmail.com>
2024-11-21 00:10:11 +0900

    241121 fetch 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Survey from './components/Survey'
 import Rechart from './components/Rechart';
+import Fetch from './components/Fetch';
 
 function App() {
 
   return (
     <>
       {/* <Survey /> */}
-      <Rechart />
+      {/* <Rechart /> */}
+      <Fetch />
     </>
   )
 }
