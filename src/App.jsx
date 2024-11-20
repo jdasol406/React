d3d2d16d8e0fb7a1ee713356cbf5de79978346ae
jdasol406 <jdasol406@gmail.com>
2024-11-20 23:16:07 +0900

    first rechart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Survey from './components/Survey'
+import Rechart from './components/Rechart';
 
 function App() {
 
   return (
     <>
-      <Survey />
+      {/* <Survey /> */}
+      <Rechart />
     </>
   )
 }
